cfaf30175c4ee3980b93f911957eb4e13d4a8e12
Umar Oluwaleke <85862102+Outsiders17711@users.noreply.github.com>
2025-11-15 17:13:02 +0100

    configure eslint with obsidian recommendations

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+// eslint.config.mjs
+import tsparser from "@typescript-eslint/parser";
+import obsidianmd from "eslint-plugin-obsidianmd";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    // global ignores
+    {
+        ignores: ["node_modules/", "main.js", "dist/"]
+    },
+    ...obsidianmd.configs.recommended,
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: tsparser,
+            parserOptions: { project: "./tsconfig.json" },
+        },
+
+        // you can add your own configuration to override or add rules
+        rules: {
+            // example: turn off a rule from the recommended set
+            "obsidianmd/sample-names": "off",
+            // example: add a rule not in the recommended set and set its severity
+            // this rule enforces using the obsidian filemanager to trash files
+            "obsidianmd/prefer-file-manager-trash-file": "error",
+        },
+    },
+]);
